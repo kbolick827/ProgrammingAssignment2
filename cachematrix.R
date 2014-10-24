d0cd861513ed68c2e5adc2eeda681b40e5142ce2
Kevin Bolick <kbolick@hotmail.com>
2014-10-23 20:43:25 -0400

    Correction
    Correction in cachesolve function

--- cachematrix.R
+++ cachematrix.R
@@ -24,7 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         
-        nv <- x$getinverse()
+        inv <- x$getinverse()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
